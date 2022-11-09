a008293e0477300df6cec713c60c5505e32ff83a
coolpiece <yyrhsc@naver.com>
2022-11-10 04:35:32 +0900

    add 로그인

---
+++ app/src/main/java/com/example/mypet/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.example.mypet;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEmailView;
+    private EditText mPasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailView = (EditText) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        // 이벤트 리스너
+        mEmailView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if(id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) return true;
+                return false;
+            }
+        });
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if(id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) return true;
+                return false;
+            }
+        });
+
+        // 버튼
+        Button mSignInButton = (Button) findViewById(R.id.sign_in_btn);
+        Button mSignUpButton = (Button) findViewById(R.id.sign_up_btn);
+
+        // 이벤트 핸들러
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
