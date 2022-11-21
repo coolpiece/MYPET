3c3977af7eb842d135a98f1d2c64fab788b3f164
coolpiece <yyrhsc@naver.com>
2022-11-21 22:32:48 +0900

    [add] intro

---
+++ app/src/main/java/com/example/mypet/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.mypet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
