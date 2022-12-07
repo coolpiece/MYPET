6c1872e81b832e69792af2801d717ddfd1189d66
coolpiece <yyrhsc@naver.com>
2022-12-08 00:36:30 +0900

    [add] AddPost

---
+++ app/src/main/java/com/example/mypet/AddPost.java
@@ -0,0 +1,18 @@
+package com.example.mypet;
+
+public class AddPost {
+    private String idToken;
+    private String title;
+    private String content;
+
+    public AddPost() { }
+
+    public String getIdToken() {return idToken;}
+    public void setIdToken(String idToken) {this.idToken = idToken;}
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
