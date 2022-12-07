a765c5ac2d1ab201d78cb706eaaf521c744e1354
sebbbin <nsb08045@daum.net>
2022-12-08 02:22:01 +0900

    hmmm

--- app/src/main/java/com/example/mypet/SignUpActivity.java
+++ app/src/main/java/com/example/mypet/SignUpActivity.java
@@ -15,8 +15,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
